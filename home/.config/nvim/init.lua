21ac573978f10e1fb1e535247f42a2c28cd036f1
hub-ry <rhubbart@purdue.edu>
2026-07-11 00:27:56 -0500

    initial commit

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,16 @@
+require('vim_config')
+
+-- Bootstrap lazy.nvim (clones it on first launch if missing)
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git', 'clone', '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load every plugin spec file under lua/plugins/
+require('lazy').setup({ import = 'plugins' })
+
